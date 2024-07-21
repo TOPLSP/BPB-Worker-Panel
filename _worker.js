7cbd7137ebf6f6cd70eb7af292f9b37ffeebc3c6
TOPLSP <38609188+TOPLSP@users.noreply.github.com>
2024-07-22 00:18:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '30701762-5a10-41aa-89c8-24808cecc536';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
